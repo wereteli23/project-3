38fef2094cbfa99ac68cb4a02ac4421fce172c94
wereteli23 <lukadeko35@gmail.com>
2024-06-05 01:32:44 -0700

    Initial Commit

---
+++ project-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project-3
+//
+//  Created by lukga dekanadze on 04.06.24.
+//
+
+import UIKit
+import SnapKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
